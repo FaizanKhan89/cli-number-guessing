164373ee3247514c6f8396a7e5a593283797cd47
Muhammad Faizan Khan <155994540+FaizanKhan89@users.noreply.github.com>
2024-03-26 01:29:29 +0500

    added a console

--- index.js
+++ index.js
@@ -2,8 +2,9 @@
 import inquirer from "inquirer";
 //1) Computer will generate a random number- Done
 //2) User input for guessing number- Done 
-//3) Compare user input with computer generated number and show result
+//3) Compare user input with computer generated number and show result-Done
 const randomNumber = Math.floor(Math.random() * 6 + 1);
+console.log("Welcome to number Guessing Game");
 const answers = await inquirer.prompt([
     {
         name: "userGuessedNumber",
--- index.ts
+++ index.ts
@@ -6,9 +6,11 @@ import inquirer from "inquirer";
 
 //2) User input for guessing number- Done 
 
-//3) Compare user input with computer generated number and show result
+//3) Compare user input with computer generated number and show result-Done
 
 const randomNumber = Math.floor(Math.random() *6 + 1);
+
+console.log("Welcome to number Guessing Game");
 
 const answers = await inquirer. prompt ([
     {
